fdf6248b53628c782eaf875b05a80cc3b0467a2d
Bernard Blackham <bernardb@ertos.nicta.com.au>
2013-01-14 13:26:37 +1100

    conflict.py: Beginning of code to do per-preemption points tests.

--- conflict.py
+++ conflict.py
@@ -201,6 +201,26 @@ def process_conflict(fout, conflict_file):
     fout.write("\n");
     f.close()
 
+def process_preemptions(fout, preemption_edges, pp_num):
+    fout.write("\ === preemption point constraints === \n\n")
+    if pp_num == None:
+        fout.write("\ No preemption points enabled.\n\n")
+    else:
+        if pp_num == 0:
+            # Leave the starting edge in.
+            fout.write("\ Assuming entry at start.\n\n")
+        else:
+            fout.write("\ Assuming entry at pp %d.\n\n" % pp_num)
+
+        # Disable all not-preempt edges from preemption points, except our
+        # starting preemption point.
+        for i, (src, preempt_dst, not_preempt_dst) in enumerate(preemption_edges):
+            if i == pp_num - 1:
+                fout.write("d%d_%d = 1\n" % (src, not_preempt_dst))
+            else:
+                fout.write("d%d_%d = 0\n" % (src, not_preempt_dst))
+    fout.write("\n")
+
 def print_constraints(conflict_file, old_cons_file, new_cons_file, pp_num):
     global bb_count
     global edge_count
@@ -208,25 +228,58 @@ def print_constraints(conflict_file, old_cons_file, new_cons_file, pp_num):
     preemption_edges = read_preemption_edges(conflict_file)
     num_pp = len(preemption_edges)
     print "Have %d preemption points" % num_pp
+    entry_pp = None
     if num_pp > 0:
         if pp_num != None:
             assert 0 <= pp_num <= num_pp
-            if pp_num == num_pp:
-                print "Exercising pp from %d -> END" % (pp_num)
-            elif pp_num == 0:
-                print "Exercising pp from START -> %d" % (pp_num + 1)
+            if pp_num == 0:
+                print "Exercising pps from START"
+                entry_pp = None
             else:
-                print "Exercising pp from %d -> %d" % (pp_num, pp_num + 1)
+                print "Exercising pps from %d" % (pp_num)
+                entry_pp = preemption_edges[pp_num - 1]
         else:
             print "But not doing anything about it. Iterate from 0 .. %d." % num_pp
+
+    # If we want to start the CFG at some other point, we need to fudge the
+    # flow equations. If we're forcing the edge A->B as our starting point, we
+    # need to set dA_B = 1, and also remove the flow equation for A's sources.
+    # Here we construct a regexp that describes the line we're looking for.
+    if entry_pp != None:
+        entry_pp_re = re.compile(r'b%d -.* d%d_%d .*= 0$' % (
+                entry_pp[0], entry_pp[0], entry_pp[1]))
+    else:
+        entry_pp_re = None
 
     fin = open(old_cons_file)
     fout = open(new_cons_file, 'w+')
 
+    start_edge_line = None
+
+    discarded_pp_line = False
     for line in fin:
+        line = line.strip()
+        if line == 'dSta_0 = 1':
+            if entry_pp != None:
+                # Force the start edge to 0.
+                fout.write('dSta_0 = 0\n')
+                continue
+
+        if entry_pp_re != None:
+            g = entry_pp_re.match(line)
+            if g:
+                if discarded_pp_line:
+                    raise Exception("Found multiple flow lines for preemption point.")
+                discarded_pp_line = True
+                continue
+
         if line.strip() == 'Generals':
             process_conflict(fout, conflict_file)
-        fout.write(line)
+            process_preemptions(fout, preemption_edges, pp_num)
+        fout.write(line + "\n")
+
+    if entry_pp_re != None and not discarded_pp_line:
+        raise Exception("Failed to find flow line for preemption point.")
 
     fin.close()
     fout.close()
